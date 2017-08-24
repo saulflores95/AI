9facda923f24201358e370caa82609a47c5554dc
Saul Flores <saulpolo95@gmail.com>
2017-08-24 14:11:23 -0700

    Made better regresion model for Ej 2 and 3

--- 8_23_2017/NN_SIN_Ej2.py
+++ 8_23_2017/NN_SIN_Ej2.py
@@ -13,11 +13,13 @@ plt.show()
 
 tnorm = tflearn.initializations.uniform(minval=-1., maxval=1.)
 input_ = tflearn.input_data(shape=[None, 1])
-hidden = tflearn.fully_connected(input_, n_units=5, activation='sigmoid', weights_init=tnorm)
-output = tflearn.fully_connected(hidden, n_units=1, activation=None, name='output')
+
+hidden = tflearn.fully_connected(input_, n_units=5, activation='tanh', weights_init=tnorm)
+hidden2 = tflearn.fully_connected(hidden, n_units=10, activation='tanh', weights_init=tnorm)
+output = tflearn.fully_connected(hidden2, n_units=1, activation=None, name='output')
 
 regression = tflearn.regression(output, optimizer='sgd', loss='mean_square',
-                                metric='R2', learning_rate=.7)
+                                metric='R2', learning_rate=.4)
 
 m = tflearn.DNN(regression)
 m.fit(X.reshape(len(X),1), Y.reshape(len(Y),1), n_epoch=100, show_metric=True, snapshot_epoch=False)
