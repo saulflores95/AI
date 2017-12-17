0b0e8915896b33e7bba1ae85c14c6384a6b7873c
Saul Flores <saulpolo95@gmail.com>
2017-12-17 13:13:53 -0800

    Fix folder structure

---
+++ machine-learning-aplicado/imdb.py
@@ -0,0 +1,8 @@
+#   Difinir politicas de
+#   financiamiento de producciones
+#   cinematograficas nacionales
+#   1. Los ingresos de una pelicula corresponden a valores continuos
+#   2. Metrica de precicion -  exito es acercarnos al valor real de ingresos
+#   3. Se basara en BD pubblicos de internet
+#   4. El dominio de trabajo es la industria del cine en particular la distribucion de peliculas
+#   5.Patrones de peliculas exitosas: Actores, Calidad, Presupuesto.
